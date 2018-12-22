e7e9a5dd60fea6ad8d62837a170f9d4c11fdbd08
AbdulDroid <olanre12waju@gmail.com>
2018-12-22 12:58:02 +0100

    Add layout files and view dependencies for Bookstore Activity

--- app/src/main/java/com/mahadum360/mahadum/bookstore/BookStoreActivity.kt
+++ app/src/main/java/com/mahadum360/mahadum/bookstore/BookStoreActivity.kt
@@ -1,13 +1,33 @@
 package com.mahadum360.mahadum.bookstore
 
-import android.support.v7.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.LayoutInflater
+import android.widget.TextView
 import com.mahadum360.mahadum.Mahadum
 import com.mahadum360.mahadum.R
+import com.mahadum360.mahadum.bookstore.adapters.BookStoreViewPagerAdapter
 import com.mahadum360.mahadum.bookstore.di.BookStoreActivityContextModule
+import com.mahadum360.mahadum.bookstore.fragments.MyBooksFragment
+import com.mahadum360.mahadum.bookstore.fragments.ParentStoreFragment
+import com.mahadum360.mahadum.models.Book
+import kotlinx.android.synthetic.main.activity_book_store.*
 import javax.inject.Inject
 
 class BookStoreActivity : AppCompatActivity(), BookStoreContract.View {
+    override fun onGetAllBooks(books: List<Book>) {
+
+    }
+
+    override fun onAddBook(status: String) {
+
+    }
+
+    override fun onBuyBook(status: String) {
+
+    }
+
     override fun hideProgress() {
 
     }
@@ -34,5 +54,34 @@ class BookStoreActivity : AppCompatActivity(), BookStoreContract.View {
                 .appComponent
                 .add(BookStoreActivityContextModule(this))
                 .inject(this)
+        initViews()
+    }
+
+    private fun initViews() {
+        setupViewPager()
+        tabs.setupWithViewPager(viewPager)
+        setupTabs()
+        backButton.setOnClickListener {
+            //todo implement back function
+        }
+    }
+
+    @SuppressLint("InflateParams")
+    private fun setupTabs() {
+        val tabOne = LayoutInflater.from(this).inflate(R.layout.custom_tab_layout,
+                null) as TextView
+        tabOne.text = ("Parent Store")
+        tabs.getTabAt(0)!!.customView = tabOne
+        val tabTwo = LayoutInflater.from(this).inflate(R.layout.custom_tab_layout,
+                null) as TextView
+        tabTwo.text = ("My Books")
+        tabs.getTabAt(1)!!.customView = tabTwo
+    }
+
+    private fun setupViewPager() {
+        val adapter = BookStoreViewPagerAdapter(supportFragmentManager)
+        adapter.addFragment(ParentStoreFragment(), "Parent Store")
+        adapter.addFragment(MyBooksFragment(), "My Books")
+        viewPager.adapter = adapter
     }
 }
