0cff7047cc829dbf9551e5d7c9d720349135aa59
AbdulDroid <olanre12waju@gmail.com>
2018-12-22 12:58:49 +0100

    Add My books fragment and Parent store fragment

---
+++ app/src/main/java/com/mahadum360/mahadum/bookstore/fragments/MyBooksFragment.kt
@@ -0,0 +1,62 @@
+package com.mahadum360.mahadum.bookstore.fragments
+
+import android.content.Context
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mahadum360.mahadum.R
+
+class MyBooksFragment : Fragment() {
+
+    private var listener: OnFragmentInteractionListener? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_my_books, container, false)
+    }
+
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is OnFragmentInteractionListener) {
+            listener = context
+        } else {
+            throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+
+    interface OnFragmentInteractionListener {
+
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @return A new instance of fragment MyBooksFragment.
+         */
+        @JvmStatic
+        fun newInstance() =
+                MyBooksFragment().apply {
+                    arguments = Bundle().apply {
+
+                    }
+                }
+    }
+}
